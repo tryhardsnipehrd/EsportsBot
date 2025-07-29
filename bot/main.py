33262f2a7cdc54812b8b4cfdbd2c3d18bd3bb28b
Holden Syverson <tryhrsnphrd@tryhrdsnphrd.com>
2025-07-28 18:00:04 -0600

    Added support to change options during runtime, and preliminary support for reaction roles

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,7 @@ import os
 CLIENT_TOKEN = os.getenv("DISCORD_TOKEN")
 DATABASE_PATH = os.getenv("DATABASE_PATH")
 REACT_MESSAGE_ID = int(os.getenv("REACT_MESSAGE_ID"))
+GUILD_ID = int(os.getenv("GUILD_ID"))
 
 # Verify that we actually got a bot token
 if CLIENT_TOKEN == None:
@@ -27,14 +28,8 @@ intents.message_content = True
 intents.messages = True
 
 client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
 
-
-# Let's connect to the database, and ensure tables are setup
-async def setup_database():
-    async with sql.connect(DATABASE_PATH) as db:
-        await db.execute("CREATE TABLE IF NOT EXISTS reacts(emote text, role int);")
-        await db.execute("CREATE TABLE IF NOT EXISTS settings(name text, data text);")
-        await db.commit()
 
 # DEBUG
 async def debug_database(table):
@@ -46,21 +41,44 @@ async def debug_database(table):
                     print(f"{item} | ", end="")
                 print()
 
-
+                
+# Let's connect to the database, and ensure tables are setup
+async def setup_database():
+    async with sql.connect(DATABASE_PATH) as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS reacts(emote text, role int);")
+        await db.execute("CREATE TABLE IF NOT EXISTS settings(name text, data text);")
+        await db.commit()
 
 async def get_settings():
     async with sql.connect(DATABASE_PATH) as db:
         # Anything we need immediately will need to be specified here.
         global LOG_CHANNEL_ID
+        global log_channel
+        
         result = await db.execute("SELECT * FROM settings WHERE name='log_channel'")
         result = await result.fetchone()
         LOG_CHANNEL_ID = int(result[1])
+        log_channel = client.get_channel(LOG_CHANNEL_ID)
         print(f"Log Channel ID Retrieved from DB: {LOG_CHANNEL_ID}")
-    
+
+async def check_setting_exists(setting: str):
+    async with sql.connect(DATABASE_PATH) as db:
+        result = await db.execute(f"SELECT * FROM settings WHERE name='{setting}'")
+        result = await result.fetchall()
+        return result
+
+async def set_setting(setting: str, value: str):
+    async with sql.connect(DATABASE_PATH) as db:
+        await db.execute(f"UPDATE settings SET data='{value}' WHERE name='{setting}'")
+        await db.commit()
+        
 @client.event
 async def on_ready():
     # This needs to be global to fix scoping issues
     global log_channel
+
+    print("Syncing App Commands")
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
 
     print(f"Logged in as {client.user}")
 
@@ -111,12 +129,12 @@ async def on_message_edit( old_message, new_message ):
     EditedEmbed = discord.Embed(
         title=f"{author_name} - <{author_id}>",
         color=0xF9B52C,
-        description=f"**Old:** {old_message.content}\n\n**+New:** {new_message.content}"
+        descripppption=f"**Old:** {old_message.content}\n\n**+New:** {new_message.content}"
     )
     EditedEmbed.set_thumbnail(url=author_pfp)
 
     await log_channel.send(embed=EditedEmbed)
-
+    
 @client.event
 async def on_raw_reaction_add( payload ):
     if payload.message_id == REACT_MESSAGE_ID:
@@ -133,6 +151,31 @@ async def on_raw_reaction_add( payload ):
                 print("Here Kitty Kitty")
             case 129370:
                 print("Egg")
+
+
+# Slash Commands
+@tree.command(
+    name="settings",
+    description="Modify the settings for the bot (PRIVILEGED)",
+    guild=discord.Object(id=GUILD_ID)
+)
+async def settings_command(interaction, setting: str, value: str):
+    results = await check_setting_exists(setting)
+    if len(results) == 0:
+        await interaction.response.send_message(f"{setting} does not exist in the database", ephemeral=True)
+        return
+    await set_setting(setting, value)
+    await interaction.response.send_message("Value was updated in Database");
+    await get_settings()
+
+
+@tree.command(
+    name="edit_react",
+    description="Add a new react, or edit an existing one (PRIVILEGED)",
+    guild=discord.Object(id=GUILD_ID)
+)  
+async def edit_react_command(interaction, emote: discord.Emoji, role: discord.Role):
+    await interaction.response.send_message("IT WORKED")
 
 
 # Start
